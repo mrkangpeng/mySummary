9b1aaef3f50e4a85d42c026fe514d118062ad2e6
mrkangpeng <mrkangpeng@foxmail.com>
2020-01-15 17:49:28 +0800

    add attribute of table

--- About Vue/Components/Table_base_elementUI/Table.vue
+++ About Vue/Components/Table_base_elementUI/Table.vue
@@ -2,8 +2,8 @@
  * @Description:'封装table' 
  * @Autor: kangpeng
  * @Date: 2019-11-26 11:26:20
- * @LastEditors: kangpeng
- * @LastEditTime: 2019-12-12 18:28:04
+ * @LastEditors  : kangpeng
+ * @LastEditTime : 2020-01-15 14:38:16
  -->
 <template>
   <div class="table_wrap">
@@ -13,14 +13,24 @@
       :data="tableData"
       :border="isShowBorder"
       :stripe="isShowStripe"
-      style="width:95%"
+      style="width:100%"
+      :cell-style="cellStyle"
+      :default-sort="{prop: 'date', order: 'descending'}"
       @selection-change="handleSelectionChange"
     >
       <el-table-column type="selection" width="45px" :align="tablePosition"></el-table-column>
       <el-table-column label="序号" width="60px" type="index" :align="tablePosition"></el-table-column>
       <template v-for="(item,index) in title">
         <slot name="img" v-if="index == positionIndex"></slot>
-        <el-table-column :prop="item.value" :label="item.label" :align="tablePosition" :key="index"></el-table-column>
+        <el-table-column
+          :show-overflow-tooltip="true"
+          :sortable="item.isSort"
+          :prop="item.value"
+          :label="item.label"
+          :align="tablePosition"
+          :key="index"
+          :width="item.width"
+        ></el-table-column>
       </template>
       <slot name="operation"></slot>
     </el-table>
@@ -49,13 +59,20 @@ export default {
     headerStyle: {
       type: Object,
       default: () => {
-        return { background: "#F9F8F8" }
+        return {
+          background:
+            "linear-gradient(0,rgba(230,235,241,1),rgba(242,243,247,1)"
+        };
       }
+    },
+    // 排序
+    sortable: {
+      type: Boolean,
+      default: false
     },
     // 定义table的高度
     tableHeight: {
-      type: Number,
-      default: 500
+      type: Number
     },
     // 对齐方式'center--居中,left--靠左,right--靠右'
     tablePosition: {
@@ -92,11 +109,32 @@ export default {
      * @author: kangpeng
      */
     handleSelectionChange(checkedList) {
-      this.$emit('getCheckList',checkedList)
+      this.$emit("getCheckList", checkedList);
+    },
+    cellStyle({ columnIndex }) {
+        return 'font-size:12px'
     }
   },
   components: {}
 };
 </script>
 
-<style scoped lang="stylus"></style>
+<style scoped lang="stylus">
+.table_wrap
+    .el-table >>> thead
+        color #000000
+    .el-table >>> td, .el-table >>> th.is-leaf, .el-table--border, .el-table--group
+        border-color #bdbdbd
+    .el-table--border::after, .el-table--group::after, .el-table::before
+        background-color #bdbdbd
+    .el-table
+        >>>.el-button
+            font-size 12px
+            padding 4px 6px
+    .el-table__header tr, >>> .el-table__header th
+        padding 0
+        height 35px
+    .el-table__body tr, >>> .el-table__body td
+        padding 0
+        height 30px
+</style>
